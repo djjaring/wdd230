33acee82b9b2483246790cab982623ae684bba95
djjaring <danjayjaring41@gmail.com>
2025-03-06 18:54:17 -0500

    commit

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,15 @@
-// Set current year
-document.getElementById("currentYear").textContent = new Date().getFullYear();
 
-// Set last modified date
-document.getElementById("lastModified").textContent = "Last Modification: " + document.lastModified;
+document.getElementById("year").textContent = new Date().getFullYear();
+
+document.addEventListener("DOMContentLoaded", function () {
+    
+    document.getElementById("year").textContent = new Date().getFullYear();
+
+    
+    let lastModifiedElement = document.getElementById("lastModified");
+    if (lastModifiedElement) {
+        lastModifiedElement.textContent = "Last Modified: " + document.lastModified;
+    } else {
+        console.error("Element with ID 'lastModified' not found.");
+    }
+});
