cb876e474272659841fed779bbda496a405ee9b3
djjaring <danjayjaring41@gmail.com>
2025-03-06 18:28:23 -0500

    commit

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,1 +1,5 @@
-document.getElementById("curremtdate").textContent=new Date().toLocaleDateString('en-us',Option);
+// Set current year
+document.getElementById("currentYear").textContent = new Date().getFullYear();
+
+// Set last modified date
+document.getElementById("lastModified").textContent = "Last Modification: " + document.lastModified;
