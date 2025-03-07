f06e84ebc542fa36587955691c6d896e79efafed
djjaring <danjayjaring41@gmail.com>
2025-03-06 19:54:45 -0500

    commit

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,7 @@
-// Get the current year and update the footer
-document.getElementById("year").textContent = new Date().getFullYear();
+window.onload = function () {
+    // Get the current year and update the footer
+    document.getElementById("year").textContent = new Date().getFullYear();
 
-// Get the last modified date and update the footer
-document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
+    // Get the last modified date and update the footer
+    document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
+};
